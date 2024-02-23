97746d25dc0c666ada5e03b8eec2dc79a3157fd7
shouguang <495140158@qq.com>
2024-02-23 19:49:32 +0800

    clear

--- program.go
+++ program.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 // 交替打印数字和字母
@@ -39,36 +38,4 @@ func PrintNumberAlphabet() {
 	}()
 	number <- true
 	<-exit
-}
-
-// 翻转字符串
-// https://github.com/lifei6671/interview-go/blob/master/question/q003.md
-func ReverseString(s string) (string, bool) {
-	str := []rune(s)
-	l := len(str)
-	if l > 5000 {
-		return "", false
-	}
-
-	for i := 0; i < l/2; i++ {
-		str[i], str[l-1-i] = str[l-1-i], str[i]
-	}
-	return string(str), true
-}
-
-// 判断两个给定的字符串排序后是否一致
-// https://github.com/lifei6671/interview-go/blob/master/question/q004.md
-func IsRegroup(s1, s2 string) bool {
-	sl1 := len([]rune(s1))
-	sl2 := len([]rune(s2))
-	if sl1 > 5000 || sl2 > 5000 || sl1 != sl2 {
-		return false
-	}
-
-	for _, v := range s1 {
-		if strings.Count(s1, string(v)) != strings.Count(s2, string(v)) {
-			return false
-		}
-	}
-	return true
 }
