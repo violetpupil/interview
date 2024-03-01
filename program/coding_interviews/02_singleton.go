79900bb8d6720eb58e06b81835b3740d56581145
instafever <495140158@qq.com>
2024-03-01 17:22:56 +0800

    singleton

--- program/coding_interviews/02_singleton.go
+++ program/coding_interviews/02_singleton.go
@@ -1,4 +1,24 @@
 package program
 
+import "sync"
+
 // 单例模式
 // https://refactoringguru.cn/design-patterns/singleton/go/example
+
+var lock = &sync.Mutex{}
+
+type single struct{}
+
+var singleInstance *single
+
+func GetInstance() *single {
+	if singleInstance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if singleInstance == nil {
+			singleInstance = &single{}
+		}
+	}
+
+	return singleInstance
+}
