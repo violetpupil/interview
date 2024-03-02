4c8a39aad9919ea458e410d9233d8ac838739e4b
instafever <495140158@qq.com>
2024-03-02 12:09:49 +0800

    GetInstance2

--- program/coding_interviews/02_singleton.go
+++ program/coding_interviews/02_singleton.go
@@ -5,19 +5,33 @@ import "sync"
 // 单例模式
 // https://refactoringguru.cn/design-patterns/singleton/go/example
 
-var lock = &sync.Mutex{}
-
 type single struct{}
 
 var singleInstance *single
 
-func GetInstance() *single {
+// 解法一
+var lock = &sync.Mutex{}
+
+func GetInstance1() *single {
 	if singleInstance == nil {
 		lock.Lock()
 		defer lock.Unlock()
 		if singleInstance == nil {
 			singleInstance = &single{}
 		}
+	}
+
+	return singleInstance
+}
+
+// 解法二
+var once sync.Once
+
+func GetInstance2() *single {
+	if singleInstance == nil {
+		once.Do(func() {
+			singleInstance = &single{}
+		})
 	}
 
 	return singleInstance
