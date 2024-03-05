46b426702adf20bae3e76467b4983af02869a3d4
shouguang <495140158@qq.com>
2024-03-05 20:55:46 +0800

    按分组反转单链表

--- program/collect/03_reverse_k_group.go
+++ program/collect/03_reverse_k_group.go
@@ -5,9 +5,6 @@ type Node struct {
 	Value int
 }
 
-// 按分组反转单链表
-func ReverseKGroup(head *Node, k int) {}
-
 // 反转单链表
 func Reverse(head *Node) *Node {
 	var pre *Node = nil
@@ -17,4 +14,38 @@ func Reverse(head *Node) *Node {
 	}
 
 	return pre
+}
+
+// 按分组反转单链表
+func ReverseKGroup(head *Node, k int) *Node {
+	first := &Node{Next: head}
+	pre := first
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return first.Next
+			}
+		}
+
+		next := tail.Next
+		head, tail = reverseGroup(head, tail)
+		pre.Next = head
+		tail.Next = next
+		pre = tail
+		head = tail.Next
+	}
+
+	return first.Next
+}
+
+func reverseGroup(head, tail *Node) (*Node, *Node) {
+	var pre *Node = nil
+	cur := head
+	for pre != tail {
+		cur.Next, pre, cur = pre, cur, cur.Next
+	}
+
+	return tail, head
 }
