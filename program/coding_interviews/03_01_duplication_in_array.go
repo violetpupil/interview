3984e1e6a7afc3aaea1eb163b867b0e501750aa0
instafever <495140158@qq.com>
2024-04-02 23:15:48 +0800

    找出数组中重复的数字

--- program/coding_interviews/03_01_duplication_in_array.go
+++ program/coding_interviews/03_01_duplication_in_array.go
@@ -6,5 +6,22 @@ func Duplicate(numbers []int, duplication *int) bool {
 		return false
 	}
 
+	for _, n := range numbers {
+		if n < 0 || n > len(numbers)-1 {
+			return false
+		}
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		for numbers[i] != i {
+			if numbers[i] == numbers[numbers[i]] {
+				*duplication = numbers[i]
+				return true
+			}
+
+			numbers[i], numbers[numbers[i]] = numbers[numbers[i]], numbers[i]
+		}
+	}
+
 	return false
 }
