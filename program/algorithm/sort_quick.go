ac7c393903f01296e23dee9fd08a1a616248c168
shouguang <495140158@qq.com>
2024-08-29 15:56:23 +0800

    QuickSort

--- program/algorithm/sort_quick.go
+++ program/algorithm/sort_quick.go
@@ -4,4 +4,21 @@ package algorithm
 // 通过比较基准pivot，将数列分区partition
 // 时间复杂度 Ο(nlogn)
 // log n 求以2为底的对数
-func QuickSort(list []int) {}
+func QuickSort(list []int) []int {
+	if len(list) <= 1 {
+		return list
+	}
+
+	pivot := list[0]
+
+	var small, big []int
+	for i := 1; i < len(list); i++ {
+		if list[i] < pivot {
+			small = append(small, list[i])
+		} else {
+			big = append(big, list[i])
+		}
+	}
+
+	return append(append(QuickSort(small), pivot), QuickSort(big)...)
+}
