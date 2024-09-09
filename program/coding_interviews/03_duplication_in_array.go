91447a4a932f59a34ee0d859c52fc26ef4503820
shouguang <495140158@qq.com>
2024-09-09 19:01:17 +0800

    FindDuplicateSpace

--- program/coding_interviews/03_duplication_in_array.go
+++ program/coding_interviews/03_duplication_in_array.go
@@ -28,3 +28,22 @@ func FindDuplicate(list []int, dup *int) bool {
 
 	return false
 }
+
+// 不修改数组找出重复的数字
+// 在一个长度为n+1的数组里的所有数字都在1~n的范围内，所以数组中至少有一个数字是重复的。
+// 请找出数组中任意一个重复的数字，但不能修改输入的数组。
+// 例如，如果输入长度为8的数组{2,3,5,4,3,2,6,7}，那么对应的输出是重复的数字2或者3。
+
+// 时间效率优先
+// 时间复杂度 O(n)
+// 空间复杂度 O(n)
+func FindDuplicateTime(list []int) int {
+	return 0
+}
+
+// 空间效率优先
+// 时间复杂度 Ο(nlogn)
+// 空间复杂度 O(1)
+func FindDuplicateSpace(list []int) int {
+	return 0
+}
