eb1fd52a9bed29aed68431af11ac4c65eb933afd
shouguang <495140158@qq.com>
2024-09-10 14:42:12 +0800

    FindDuplicate

--- program/coding_interviews/03_duplication_in_array.go
+++ program/coding_interviews/03_duplication_in_array.go
@@ -4,30 +4,29 @@ package program
 // 在一个长度为n的数组里的所有数字都在0~n-1的范围内。
 // 数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。
 // 请找出数组中任意一个重复的数字。例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是重复的数字2或者3。
-func FindDuplicate(list []int, dup *int) bool {
+func FindDuplicate(list []int) int {
 	if len(list) == 0 {
-		return false
+		return -1
 	}
 
-	// 元素不合法，也返回false
+	// 元素不合法
 	for _, v := range list {
 		if v < 0 || v >= len(list) {
-			return false
+			return -2
 		}
 	}
 
 	for i := 0; i < len(list); i++ {
 		for list[i] != i {
 			if list[i] == list[list[i]] {
-				*dup = list[i]
-				return true
+				return list[i]
 			} else {
 				list[i], list[list[i]] = list[list[i]], list[i]
 			}
 		}
 	}
 
-	return false
+	return -1
 }
 
 // 不修改数组找出重复的数字
--- program/coding_interviews/03_duplication_in_array_test.go
+++ program/coding_interviews/03_duplication_in_array_test.go
@@ -6,9 +6,7 @@ import (
 
 func TestFindDuplicate(t *testing.T) {
 	list := []int{2, 3, 1, 0, 2, 5, 3}
-	var dup int
-	r := FindDuplicate(list, &dup)
-	t.Log(r, dup)
+	t.Log(FindDuplicate(list))
 }
 
 func TestFindDuplicateSpace(t *testing.T) {
